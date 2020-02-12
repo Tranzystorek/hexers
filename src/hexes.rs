d5438e3d7c8f60902c86dd9ee679f049232ba29c
Marcin Puc <mpuc@vmware.localdomain>
2020-02-12 21:31:03 +0100

    Move HexedByte to the aux module

--- src/hexes.rs
+++ src/hexes.rs
@@ -1,16 +1,7 @@
 use std::slice::Iter;
 use std::iter::FusedIterator;
 
-enum HexState {
-    Unread,
-    LeftRead,
-    RightRead
-}
-
-struct HexedByte<'a> {
-    byte: &'a u8,
-    state: HexState
-}
+use crate::aux::HexedByte;
 
 /// Iterator that transforms the underlying `&[u8]` sequence
 /// into a lowercase hex encoded sequence.
@@ -37,62 +28,6 @@ pub struct Hexes<'a> {
 /// ```
 pub fn hexes(slice: &[u8]) -> Hexes {
     Hexes::from_slice(slice)
-}
-
-impl HexState {
-    pub fn advance(&mut self) {
-        *self = match self {
-            HexState::Unread => HexState::LeftRead,
-            _ => HexState::RightRead
-        }
-    }
-}
-
-impl<'a> HexedByte<'a> {
-    pub fn from_ref(byte: &'a u8) -> HexedByte<'a> {
-        HexedByte::<'a> {
-            byte,
-            state: HexState::Unread
-        }
-    }
-
-    fn get_left(&self) -> u8 {
-        self.byte >> 4
-    }
-
-    fn get_right(&self) -> u8 {
-        self.byte & 0xf
-    }
-}
-
-impl<'a> Iterator for HexedByte<'a> {
-    type Item = u8;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let result = match self.state {
-            HexState::Unread => Some(self.get_left()),
-            HexState::LeftRead => Some(self.get_right()),
-            HexState::RightRead => None
-        };
-
-        self.state.advance();
-
-        result
-    }
-}
-
-impl<'a> DoubleEndedIterator for HexedByte<'a> {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        let result = match self.state {
-            HexState::Unread => Some(self.get_right()),
-            HexState::LeftRead => Some(self.get_left()),
-            HexState::RightRead => None
-        };
-
-        self.state.advance();
-
-        result
-    }
 }
 
 impl<'a> Hexes<'a> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod aux;
 mod hexes;
 pub use hexes::{Hexes, hexes};
 
